a07787892c423d5fa5a9b196f964d7b2bacea1e6
Charles Chege <charleschege@protonmail.ch>
2019-07-04 11:00:37 +0300

    Add documentation on example use

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,18 @@
 
 //! This are Custom Enum codes for memorable and uniform response codes.
 //! Enums are cheaper to compair than strings thereby guaranteeing efficiency.
-//! 
+//! ```
+//! # use custom_codes::{DbOps, CustomBool};
+//! # fn dbop () -> DbOps {
+//! #   if db_insert() == true {
+//! #       DbOps::Inserted
+//! #   }else if db_inser() == false {
+//! #       DbOps::Skipped
+//! #   }else {
+//! #       DbOps::ConnRefused
+//! #   }
+//! # }
+//! ```
 
 pub use custom_codes::{
     DbOps, 
